31f23be59905d4a33ca7cdf893ae05453ea41385
Jennifer Dobak <jdobak1@gmail.com>
2018-05-05 16:47:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,4 +20,7 @@ const driversByRevenue = function (drivers) {
 
 const driversByName = function (drivers) {
   return drivers.slice().sort(function(a, b){return a.name.localeCompare(b.name)})
+
+const totalRevenue = function(drivers) {
+  return drivers.reduce((total, amount) => total.revenue + amount.revenue)
 }
