a4600373d176debd2adf0f2b6ed03d3d911f664a
Jennifer Dobak <jdobak1@gmail.com>
2018-05-05 15:46:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution in this file!
 function logDriverNames(drivers) {
-  console.log(drivers.forEach(function(driver){
+  return drivers.forEach(function(driver){
     return driver.name
   }))
 }
